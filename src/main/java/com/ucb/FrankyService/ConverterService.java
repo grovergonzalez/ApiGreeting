3c398d09886d22a638dbf32bec17dcc8d1200478
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2024-09-15 20:17:15 -0400

    greet api and application.yml is modified

--- src/main/java/com/ucb/FrankyService/ConverterService.java
+++ src/main/java/com/ucb/FrankyService/ConverterService.java
@@ -1,6 +1,5 @@
 package com.ucb.FrankyService;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
---
+++ src/main/java/com/ucb/FrankyService/GreetingController.java
@@ -0,0 +1,29 @@
+package com.ucb.FrankyService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greet")
+public class GreetingController {
+    @Autowired
+    GreetingService greetingService;
+    @GetMapping
+    public String greeting(){
+
+        return greetingService.getGreeting();
+    }
+    @GetMapping("/withName")
+    public String greetingWithName(@RequestParam String name){
+
+        return greetingService.getGreetingWithName(name);
+    }
+    @GetMapping("/language")
+    public String greetingInEnglish(String lang){
+
+        return greetingService.getGreetingInEnglish(lang);
+    }
+}
